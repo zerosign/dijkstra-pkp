c2b9809a8b8c444f55ad8df82416da159351742e
r1nlx0 <r1nlx0@gmail.com>
2012-10-21 17:29:52 +0700

    rename reader to file

---
+++ src/file.cpp
@@ -0,0 +1,103 @@
+#include "file.h"
+
+#define BUFFSIZE 100
+
+namespace io {
+	
+				
+	file::file() {}
+		
+	void file::read(const char * filename, 
+			g::adjmap& adjmap) {
+		
+		std::ifstream input(std::string(filename).c_str());
+		
+		if(!input.good()) {
+			std::fprintf(stderr, "[ERROR] No such file %s\n", 
+					std::string(filename).c_str());
+			exit(EXIT_FAILURE);
+		}
+		char * buffer = new char[BUFFSIZE];
+
+		input.getline(buffer, BUFFSIZE);
+		
+		int num_vertices, num_edges;
+		
+		std::sscanf(buffer, "%d\t%d\n", &num_vertices,
+				&num_edges);
+
+	
+	//	std::printf("V : %d, E : %d\n", num_vertices, num_edges);
+
+		//sstream >> num_vertices >> num_edges;
+		
+		for(int ii = 0; ii < num_edges; ii++) {
+			int start, end;
+			float weight;
+			//sstream >> start >> end >> weight;
+			buffer = new char[BUFFSIZE];
+
+			input.getline(buffer, BUFFSIZE);
+		
+			std::sscanf(buffer, "%d\t%d\t%f\n", &start,
+					&end, &weight);
+
+			adjmap[start].push_back(g::neighbor(end, weight));
+			adjmap[end].push_back(g::neighbor(start, weight));
+		}
+
+	
+		input.close();
+	}
+
+	void file::read(const char * filename, 
+			float * VertexArray, float * WeightArray) {
+			
+		std::ifstream input(std::string(filename).c_str());
+		
+		if(!input.good()) {
+			std::fprintf(stderr, "[ERROR] No such file %s\n", 
+					std::string(filename).c_str());
+			exit(EXIT_FAILURE);
+		}
+		char * buffer = new char[BUFFSIZE];
+
+		input.getline(buffer, BUFFSIZE);
+		
+		int num_vertices, num_edges;
+		
+		std::sscanf(buffer, "%d\t%d\n", &num_vertices,
+				&num_edges);
+
+		VertexArray = malloc(sizeof(float)*num_vertices);	
+		WeightArray = malloc(sizeof(float)*(num_vertices*num_vertices));
+		
+		for(int ii = 0; ii < num_vertices; ii++) {
+			VertexArray[ii] = ii;
+		}
+
+		for(int ii = 0; ii < num_edges; ii++) {
+			int start, end;
+			float weight;
+			//sstream >> start >> end >> weight;
+			buffer = new char[BUFFSIZE];
+
+			input.getline(buffer, BUFFSIZE);
+		
+			std::sscanf(buffer, "%d\t%d\t%f\n", &start,
+					&end, &weight);
+
+			WeightArray[start * num_vertices + end ] = weight;
+		}
+
+	
+		input.close();
+	
+	}
+
+	void file::write(const char * filename,
+		std::string output) {
+
+	}
+	
+};
---
+++ src/file.h
@@ -0,0 +1,21 @@
+#ifndef READER_H
+#define READER_H
+#include <string>
+#include <fstream>
+#include <ios>
+#include <cstdlib>
+#include "defs.h"
+
+namespace io {
+
+	class file {
+		public:
+			file();
+			static void read(const char*, g::adjmap&);
+			static void read(const char*, float *,
+				float *);
+			static void write(const char*, std::string);
+	};
+};
+
+#endif
