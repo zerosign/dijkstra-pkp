7e4c5059bf00d73c47e1a23c3c9edaa061abb783
r1nlx0 <r1nlx0@gmail.com>
2012-10-22 09:50:15 +0700

    Not recommended to be used, some changes

--- src/parallel_dijkstra.cu
+++ src/parallel_dijkstra.cu
@@ -3,8 +3,18 @@
 #include "defs.h"
 #include "reader.h"
 
-__device__ int thread_id() {
-	return threadIdx.x;
+
+
+__device__ int index() {
+	return blockIdx.x * blockDim.x + threadIdx.x;
+}
+
+__device__ int x(int index) {
+	return index;
+}
+
+__device__ int y(int index) {
+	return index;
 }
 
 __global__ void first_cuda_ssp_kernel() {
@@ -15,21 +25,34 @@ __global__ void second_cuda_ssp_kernel() {
 
 }
 
-
+/**
+void AdjMapToMatrix(g::adjmap & graph, 
+	float * VertexArray,
+	float * WeightArray);
+**/
 
 int main(int argc, char ** argv) {
 	
+	if(argc != 4) {
+		std::fprintf(stderr, "\n[Usage] : %s [filename]\n", 
+			std::string(argv[0]).c_str());
+	}
+
 	const char * filename = argv[1];
 
-	std::adjmap graph;
+	//g::adjmap graph;
 
-	g::reader::read(filename, graph);
+	//g::reader::read(std::string(filename).c_str(), graph);
 
-	int * VertexArray, * EdgeArray;
+	int * VertexArray, * EdgeArray, * MaskArray;
 	float * WeightArray, * CostArray, * UpdateCostArray;
-	bool * MaskArray;
-
 	
+	VertexArray = malloc(sizeof(
+
+	AdjMapToMatrix(graph, VertexArray, WeightArray);
+
 
 	return 0;
 }
+
+
