48647602ac46bfaa1dd6d8a92c6baca526524cb8
Yuri Setiantoko <r1nlx0@gmail.com>
2012-10-22 19:46:49 +0700

    I hope this done

--- src/file.h
+++ src/file.h
@@ -12,7 +12,7 @@ namespace io {
 		public:
 			file();
 			static void read(const char*, g::adjmap&);
-			static void read(const char*,	int *&, float *&);
+			static void read(const char*,	int &, float *&);
 			static void write(const char*, std::string);
 	};
 };
